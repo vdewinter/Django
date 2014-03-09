9042d8fcf8501e040419cef9e1c78c9826fdd13d
Victoria de Winter <dewinter.victoria@gmail.com>
2014-03-09 11:53:57 -0700

    Added tests

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,14 @@ class Poll(models.Model):
 	def __unicode__(self):  # Python 3: def __str__(self):
 		return self.question
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date < now
+	def was_published_recently_with_old_poll(self):
+		old_poll = Poll(pub_date=timezone.now())
+		self.assertEqual(old_poll.was_published_recently(), False)
+	def test_was_published_recently_with_recent_poll(self):
+		recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+		self.assertEqual(recent_poll.was_published_recently(), True)
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
